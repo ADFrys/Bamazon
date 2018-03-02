beed4c8519d929edc771eed6aabb6f938f8b2ac5
AngelaF <ADFrys@users.noreply.github.com>
2018-03-02 13:17:14 -0600

    Update table schema.

--- BamazonSchema.sql
+++ BamazonSchema.sql
@@ -16,10 +16,6 @@ INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("keyboard", "electronics", 24.99, 100), ("mouse", "electronics", 9.99, 200), 
 ("laptop", "electronics", 1000, 50), ("vitamins", "health", 10.25, 500), ("dog food", "pet supplies", 30, 200), 
 ("cat food", "pet supplies", 24.99, 300), ("bandages", "health", 3.99, 900), ("sweatshirt", "clothing", 24.99, 10), 
-("jeans", "clothing", 29.99, 20), ("t-shirt", "clothing", 9.99, 35);
+("jeans", "clothing", 29.99, 20), ("t-shirt", "clothing", 9.99, 35), ("pencil", "office supplies", 1.99, 1);
 
-** This is the sql query for the function which checks the stock quantity. 
-UPDATE products
-SET stock_quantity = stock_quantity - 1
-WHERE item_id = 1
 
